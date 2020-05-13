eeefdcf759b0a383ec49eb2a7c0b8c504199c2f3
Nastassia Chaliapina <Nastassia_Chaliapina@epam.com>
2020-05-13 11:41:00 +0300

    rrr

---
+++ src/test/java/driver/DriverSingleton.java
@@ -0,0 +1,39 @@
+package driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverSingleton {
+
+   private static final String RESOURCE_PATH = "src\\test\\java\\recources\\";
+    protected static WebDriver driver;
+
+    protected DriverSingleton() {
+    }
+
+    public static WebDriver getDriver() {
+        if (null == driver) {
+            switch (System.getProperty("browser")) {
+                case "firefox": {
+                    //System.setProperty("webdriver.gecko.driver", RESOURCE_PATH + "geckodriver.exe");
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+                default: {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
+            }
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+
+    public static void closeDriver() {
+        driver.quit();
+        driver = null;
+    }
+}
